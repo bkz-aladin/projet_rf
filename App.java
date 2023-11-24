0c3e659de5b1dc44914f11ef36fe37d6aafad3b8
bkz-aladin <yaya.latif1999@gmail.com>
2023-11-24 01:13:56 +0100

    Lecture des différentes mesurses

--- App.java
+++ App.java
@@ -5,34 +5,37 @@ public class App {
         int nombreClasses = 9;
         int nombreImagesParClasse = 11;
         int nombreMesuresE34 = 16;
+        int nombreMesuresF0 = 128;
+        int nombreMesuresGFD = 100;
+        int nombreMesuresSA = 90;
 
         // Déclaration et initialisation d'un tableau à trois dimensions
         double[][][] mesuresE34 = new double[nombreClasses][nombreImagesParClasse][nombreMesuresE34];
+        double[][][] mesuresF0 = new double[nombreClasses][nombreImagesParClasse][nombreMesuresF0];
+        double[][][] mesuresGFD = new double[nombreClasses][nombreImagesParClasse][nombreMesuresGFD];
+        double[][][] mesuresSA = new double[nombreClasses][nombreImagesParClasse][nombreMesuresSA];
 
         // Création d'une instance de LecteurDonnees pour les mesures E34
-        LecteurDonnees lecteurE34 = new LecteurDonnees("../data/E34/", nombreMesuresE34);
+        LecteurDonnees lecteurE34 = new LecteurDonnees("../data/E34/", nombreMesuresE34, ".e34");
+        LecteurDonnees lecteurF0 = new LecteurDonnees("../data/F0/", nombreMesuresF0, ".f0");
+        LecteurDonnees lecteurGFD = new LecteurDonnees("../data/GFD/", nombreMesuresGFD, ".gfd");
+        LecteurDonnees lecteurSA = new LecteurDonnees("../data/SA/", nombreMesuresSA, ".sa");
 
         // Utilisation de l'instance pour lire les mesures E34
         lecteurE34.lireMesures(mesuresE34);
+        lecteurF0.lireMesures(mesuresF0);
+        lecteurGFD.lireMesures(mesuresGFD);
+        lecteurSA.lireMesures(mesuresSA);
 
         // System.out.println(mesuresE34);
-        // Affichage des mesures E34
-        // lecteurE34.lireMesures(mesuresE34);
+        // lecteurE34.afficherMesures(mesuresE34);
+        // lecteurF0.afficherMesures(mesuresF0);
+        lecteurGFD.afficherMesures(mesuresGFD);
+        // lecteurSA.afficherMesures(mesuresSA);
 
         // Affichage du tableau
-        afficherTableau(mesuresE34);
+        // afficherTableau(mesuresE34);
     }
 
-    private static void afficherTableau(double[][][] tableau) {
-        System.out.println("Affichage du tableau de mesures :");
-        for (int classe = 0; classe < tableau.length; classe++) {
-            for (int image = 0; image < tableau[classe].length; image++) {
-                for (int mesure = 0; mesure < tableau[classe][image].length; mesure++) {
-                    System.out.print(tableau[classe][image][mesure] + " ");
-                }
-                System.out.println(); // Passer à la ligne après chaque ligne d'image
-            }
-            System.out.println(); // Passer à la ligne après chaque classe
-        }
-    }
+    
 }
--- LecteurDonnees.java
+++ LecteurDonnees.java
@@ -7,10 +7,12 @@ import java.util.List;
 public class LecteurDonnees {
 
     private String cheminVersFichier;
+    private String extension;
     private int nombreMesures;
 
-    public LecteurDonnees(String cheminVersFichier, int nombreMesures) {
+    public LecteurDonnees(String cheminVersFichier, int nombreMesures, String extension) {
         this.cheminVersFichier = cheminVersFichier;
+        this.extension = extension;
         this.nombreMesures = nombreMesures;
     }
 
@@ -40,23 +42,27 @@ public class LecteurDonnees {
     public void lireMesures(double[][][] tableauMesures) {
         for (int classe = 0; classe < tableauMesures.length; classe++) {
             for (int image = 0; image < tableauMesures[classe].length; image++) {
-                tableauMesures[classe][image] = lireMesure(cheminVersFichier + "S" + String.format("%02d", classe + 1) + "N" + String.format("%03d", image + 1) + ".e34");
+                tableauMesures[classe][image] = lireMesure(cheminVersFichier + "S" + String.format("%02d", classe + 1) + "N" + String.format("%03d", image + 1) + this.extension);
             }
         }
     }
 
     // Méthode pour afficher le tableau
-    public void afficherTableau(double[][][] tableau) {
+    public void afficherMesures(double[][][] tableau) {
         System.out.println("Affichage du tableau de mesures :");
         for (int classe = 0; classe < tableau.length; classe++) {
+
+            System.out.println("classe " + (classe+1));
             for (int image = 0; image < tableau[classe].length; image++) {
+
                 for (int mesure = 0; mesure < tableau[classe][image].length; mesure++) {
+
                     System.out.print(tableau[classe][image][mesure] + " ");
                 }
                 System.out.println(); // Passer à la ligne après chaque ligne d'image
-                System.out.println("classe numero : " + classe + " image numéro " + image);
             }
             System.out.println(); // Passer à la ligne après chaque classe
+            // System.out.println("classe " + (classe+1)); 
         }
     }
 }
